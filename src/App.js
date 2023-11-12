fd8937437f9151b2b20cb2be5db9740ccbcda54d
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2023-11-12 15:24:44 +0300

    feat: add component Header in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import './scss/App.scss';
+import Header from './components/header/Header';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header/>
     </div>
   );
 }
