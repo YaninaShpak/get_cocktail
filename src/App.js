37b400732e3d40141ff23161e6604cb62d78f00e
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2023-11-13 15:19:01 +0300

    feat: create draft html-code, add CocktailCard component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
+//styles
 import './scss/App.scss';
+
+//components
 import Header from './components/header/Header';
+import CocktailCard from './components/cocktailCard/CocktailCard';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <div className="wrapper">
+        <Header />
+        <main className="main-content">
+          <div className="container content-container">
+            <section className="filters content-container__filters">
+              <h2 className="title filters__title">Filters</h2>
+            </section>
+            <div className="actions-wrapper content-container__actions">
+              <div className="sorting">
+                <h2 className="title sorting__title">Sorting</h2>
+              </div>
+              <div className="search">
+                <input type="search" />
+              </div>
+              <button className="btn-random" type="button">Random cocktail</button>
+            </div>
+            <section className="cocktails content-container__cocktails">
+              <h2 className="visually-hidden">Cocktails list</h2>
+              <ul className="cocktails-list list-reset cocktails__list">
+                <CocktailCard />
+                <CocktailCard />
+                <CocktailCard />
+              </ul>
+            </section>
+          </div>
+        </main>
+      </div>
     </div>
   );
 }
