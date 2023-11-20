21ed4d65fba54cab2c595666d89860b6d26f63c2
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2023-11-20 21:00:46 +0300

    feat: code re-write with RTK

--- src/components/categoriesFilter/CategoriesFilter.jsx
+++ src/components/categoriesFilter/CategoriesFilter.jsx
@@ -1,16 +1,24 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { setCurrentCategory } from '../../redux/slices/filterSlice';
+
 //styles
 import styles from './CategoriesFilter.module.scss';
 
 const categories = ['All', 'Alcoholic', 'Non-alcoholic'];
 
 const CategoriesFilter = () => {
+  const dispatch = useDispatch();
+  const currentCategory = useSelector((state) => state.filter.category);
+
   return (
     <div className='categories'>
       <ul className={`${styles.categoriesList} list-reset`}>
         {categories.map((category, index) => (
           <li
             key={index}
-            className={styles.categoriesList__item}>
+            className={`${styles.categoriesList__item} ${currentCategory === index ? styles.current : '' }`}
+            onClick={() => dispatch(setCurrentCategory(index))}
+          >
             {category}
           </li>
         ))}
