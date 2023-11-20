bd2733c816b611b953396ca5403c67b8ff026fb7
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2023-11-20 21:09:28 +0300

    refactor: change name imported component

--- src/components/filters/Filters.jsx
+++ src/components/filters/Filters.jsx
@@ -3,14 +3,16 @@ import styles from './Filters.module.scss';
 
 //components
 import CategoriesFilter from '../categoriesFilter/CategoriesFilter';
-import RadioBtnFilter from '../radiobtnFilter/RadioBtnFilter';
+import RadioFilter from '../radioFilter/RadioFilter';
 
 const Filters = () => {
   return (
     <section className={`${styles.filters} content-container__filters`}>
       <h2 className={`title ${styles.filters__title}`}>Filters</h2>
-      <CategoriesFilter />
-      <RadioBtnFilter />
+      <div className={styles.filters__wrapper}>
+        <CategoriesFilter />
+        <RadioFilter />
+      </div>
     </section>
   );
 };
