364720cf649f5563fbc882d050f288c4b46bf09e
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2023-11-20 21:03:08 +0300

    feat: create store and slice from RTK

---
+++ src/redux/slices/filterSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  category: 0,
+  baseIngredient: '',
+};
+
+export const filterSlice = createSlice({
+  name: 'filter',
+  initialState,
+  reducers: {
+    setCurrentCategory: (state, action) => {
+      state.category = action.payload
+    },
+    setBaseIngredient: (state, action) => {
+      state.baseIngredient = action.payload
+    },
+  }
+});
+
+export const { setCurrentCategory, setBaseIngredient } = filterSlice.actions;
+export default filterSlice.reducer;
