c6d5a6426b6609e926b7c8e65a802380a548142d
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2023-11-22 16:36:32 +0300

    feat: create sortingSlice

---
+++ src/redux/slices/sortingSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  sorting: { nameItem: 'popularity', nameSort: '' }
+};
+
+export const sortingSlice = createSlice({
+  name: 'sort',
+  initialState,
+  reducers: {
+    setSorting: (state, action) => {
+      state.sorting = action.payload
+    }
+  }
+});
+
+export const { setSorting } = sortingSlice.actions;
+export default sortingSlice.reducer;
