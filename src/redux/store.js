c0f251d0faee3aa6eaa7a3a569417ad15e057fca
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2023-12-06 15:29:05 +0300

    feat: add searchSlice in store (RTX)

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import filterSlice from "./slices/filterSlice";
 import sortingSlice from "./slices/sortingSlice";
+import searchSlice from "./slices/searchSlice";
 
 export const store = configureStore({
   reducer: {
     filter: filterSlice,
-    sort: sortingSlice
+    sort: sortingSlice,
+    search: searchSlice
   },
 });
