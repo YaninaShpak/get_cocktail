406d481f7308903639c722f9d5ad343820d230e4
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2023-12-12 15:38:48 +0300

    feat: create component RandomCocktailButton

---
+++ src/components/buttons/RandomCocktailButton/RandomCocktailButton.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+//styles
+import styles from './RandomCocktailButton.module.scss';
+
+const RandomCocktailButton = () => {
+  return (
+    <Link to="random-cocktail" className={styles.button}>
+      Random cocktail
+    </Link>
+  );
+};
+
+export default RandomCocktailButton;
