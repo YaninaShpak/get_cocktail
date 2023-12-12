e88f1723586d253bf2b0b813f1e55afe63ef48e9
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2023-12-12 15:41:12 +0300

    fix: replace tag a with Link (Router)

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+//styles
 import style from './Header.module.scss';
 
 const Header = () => {
   return (
     <header className={style.mainHeader}>
-      <h1 className={style.mainHeader__title}>
-        <a className="link" href='/'>Let's get cocktail!</a>
-      </h1>
+      <p className={style.mainHeader__title}>
+        <Link className="link" to="/">Let's get cocktail!</Link>
+      </p>
     </header>
   );
 };
