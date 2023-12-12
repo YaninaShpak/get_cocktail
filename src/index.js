8c5d00ad417aaf122b3620d445ea2d740bda8c2b
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2023-12-12 15:36:14 +0300

    feat: connect Router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import { BrowserRouter } from 'react-router-dom';
+
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
 
@@ -9,6 +11,8 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>
 );
