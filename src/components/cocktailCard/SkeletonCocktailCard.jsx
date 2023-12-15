6a33be3910f5612a38ecd27d19ebb2d31279fd81
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2023-12-15 12:26:37 +0300

    fix: change component SkeletonCocktailCard

--- src/components/cocktailCard/SkeletonCocktailCard.jsx
+++ src/components/cocktailCard/SkeletonCocktailCard.jsx
@@ -1,20 +1,19 @@
-import React from 'react';
-import ContentLoader from 'react-content-loader';
+import React from "react"
+import ContentLoader from "react-content-loader"
 
-const SkeletonCocktailCard = (props) => {
-  return (
-    <ContentLoader 
-      speed={2}
-      width={400}
-      height={350}
-      viewBox="0 0 400 350"
-      backgroundColor="#6b6b6b"
-      foregroundColor="#b0b0b0"
-      {...props}
-    >
-      <rect x="0" y="0" rx="10" ry="10" width="260" height="335" />
+const SkeletonCocktailCard = (props) => (
+  <ContentLoader 
+    speed={2}
+    width={1340}
+    height={705}
+    viewBox="0 0 1340 705"
+    backgroundColor="#6b6b6b"
+    foregroundColor="#b0b0b0"
+    {...props}
+  >
+    <rect x="0" y="0" rx="10" ry="10" width="580" height="704" /> 
+    <rect x="630" y="0" rx="10" ry="10" width="700" height="705" />
   </ContentLoader>
-  );
-};
+)
 
 export default SkeletonCocktailCard;
