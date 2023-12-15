515436f298400af16ffa702f1d7b95329b958ee4
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2023-12-15 12:25:48 +0300

    feat: creat component CocktailCardMini

---
+++ src/components/cocktailCardMini/CocktailCardMini.jsx
@@ -0,0 +1,30 @@
+import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { setCocktailID } from '../../redux/slices/cocktailListSlice';
+
+//styles
+import style from './CocktailCardMini.module.scss';
+
+const CocktailCardMini = ({link, title, imgUrl, strength}) => {
+  const dispatch = useDispatch();
+
+  return (
+    <li>
+      <Link 
+        className={`${style.cardMini} link`} 
+        to={`cocktail/${link}`} 
+        onClick={() => dispatch(setCocktailID(link))}
+      >
+        <h3 className={style.cardMini__title}>{title}</h3>
+        <div className={style.cardMini__img}>
+          <img src={imgUrl} alt={title}/>
+        </div>
+        <div className="card__info">
+          {strength && <p>Strength: <span>{strength} %</span></p>}
+        </div>
+      </Link>
+    </li>
+  );
+};
+
+export default CocktailCardMini;
