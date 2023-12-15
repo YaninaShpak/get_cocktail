c75f6505ebf9d562dba61dacd0d3811bf97089bf
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2023-12-15 12:11:19 +0300

    feat: add cocktailListSlice (RTK)

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,11 +3,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import filterSlice from "./slices/filterSlice";
 import sortingSlice from "./slices/sortingSlice";
 import searchSlice from "./slices/searchSlice";
+import cocktailListSlice from "./slices/cocktailListSlice";
 
 export const store = configureStore({
   reducer: {
     filter: filterSlice,
     sort: sortingSlice,
-    search: searchSlice
+    search: searchSlice,
+    cocktailList: cocktailListSlice
   },
 });
