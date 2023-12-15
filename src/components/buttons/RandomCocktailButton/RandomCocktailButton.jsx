4e8c501e45759412646e01f4c0f3f0ac3cdf3f7c
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2023-12-15 12:18:46 +0300

    fix: add logic of getting random element

--- src/components/buttons/RandomCocktailButton/RandomCocktailButton.jsx
+++ src/components/buttons/RandomCocktailButton/RandomCocktailButton.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+
+import { useSelector } from 'react-redux';
+
+import { setCocktailID } from "../../../redux/slices/cocktailListSlice";
 
 //styles
-import styles from './RandomCocktailButton.module.scss';
+import styles from "./RandomCocktailButton.module.scss";
 
 const RandomCocktailButton = () => {
+  const dispatch = useDispatch();
+  const {items} = useSelector((state) => state.cocktailList);
+
+  function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  const randomID = items[getRandomIntInclusive(0, 99)]?.id;
+ 
   return (
-    <Link to="random-cocktail" className={styles.button}>
+    <Link
+      to={`random-cocktail/${randomID}`}
+      className={styles.button}
+      onClick={() => dispatch(setCocktailID(randomID))}
+    >
       Random cocktail
     </Link>
   );
