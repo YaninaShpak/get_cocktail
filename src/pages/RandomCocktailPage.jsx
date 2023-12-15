700ea353a0b58619d97e4d5ac0566869c360074c
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2023-12-15 12:12:05 +0300

    feat: creat page RandomCocktailPage

---
+++ src/pages/RandomCocktailPage.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { useSelector } from 'react-redux';
+
+//components
+import CocktailCard from "../components/cocktailCard/CocktailCard";
+import SkeletonCocktailCard from "../components/cocktailCard/SkeletonCocktailCard";
+import BackButton from "../components/buttons/BackButton/BackButton";
+
+const RandomCocktailPage = () => {
+  const {cocktailID} = useSelector((state) => state.cocktailList);
+  const {items} = useSelector((state) => state.cocktailList);
+  const item = items.find((el) => el.id === cocktailID);
+
+  return (
+    <div className="container cocktailPage">
+      {item ? <CocktailCard item={item} /> : <SkeletonCocktailCard />}
+      <BackButton/>
+    </div>
+  );
+};
+
+export default RandomCocktailPage;
