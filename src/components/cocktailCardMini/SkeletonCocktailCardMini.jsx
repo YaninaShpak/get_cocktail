87a88f001645acd0e52d3b02f59dbd03816269ee
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2023-12-15 12:24:56 +0300

    feat: create SkeletonCocktailCardMini

---
+++ src/components/cocktailCardMini/SkeletonCocktailCardMini.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ContentLoader from 'react-content-loader';
+
+const SkeletonCocktailCardMini = (props) => {
+  return (
+    <ContentLoader 
+      speed={2}
+      width={400}
+      height={350}
+      viewBox="0 0 400 350"
+      backgroundColor="#6b6b6b"
+      foregroundColor="#b0b0b0"
+      {...props}
+    >
+      <rect x="0" y="0" rx="10" ry="10" width="260" height="335" />
+  </ContentLoader>
+  );
+};
+
+export default SkeletonCocktailCardMini;
