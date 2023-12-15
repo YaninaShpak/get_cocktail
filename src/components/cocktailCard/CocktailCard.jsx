8540d69160a40d5f5c1c6dfa50b2d2dcd42e6e32
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2023-12-15 12:23:40 +0300

    feat: update component CocktailCard

--- src/components/cocktailCard/CocktailCard.jsx
+++ src/components/cocktailCard/CocktailCard.jsx
@@ -1,18 +1,44 @@
-//styles
-import style from './CocktailCard.module.scss';
+import React from 'react';
 
-const CocktailCard = ({title, imgUrl, strength}) => {
-  return (
-    <li className={style.card}>
-      <h3 className={style.card__title}>{title}</h3>
-      <div className={style.card__img}>
-        <img src={imgUrl} alt={title}/>
+//components
+import CocktailProperty from '../cocktailProperty/CocktailProperty';
+
+//styles
+import styles from './CocktailCard.module.scss';
+
+const CocktailCard = ({item}) => {
+    return (
+        <div className={styles.root}>
+        <div className={styles.root__info}>
+          <h1>{item.Title}</h1>
+          <div className={styles.root__ingredients}>
+            <h2>Ingredients</h2>
+            <ul className={`${styles.ingredientsList} list-reset`}>
+            {item.Ingredients?.map((el, i) => 
+              <li className={styles.ingredientsList__item} key={item.id + i}>
+                <p className={styles.ingredientsList__name}>{el}</p>
+                <p className={styles.ingredientsList__amount}>{item.Measures[i]}</p>
+              </li>
+            )}
+            </ul>
+          </div>
+          <div className={styles.root__instruction}>
+            <h2>Instructions</h2>
+            <p>{item.Instructions}</p>
+          </div>
+          <CocktailProperty title="Alcoholic" desc={item.Alcoholic} />
+          <CocktailProperty title="Category" desc={item.Category} />
+          <CocktailProperty title="Glass" desc={item.Glass} />
+          {item.totalStrength &&
+            <CocktailProperty title="TotalStrength" desc={item.totalStrength} />
+          }
+        </div>
+        <div className={styles.root__img}>
+          <img src={item.Img} alt={item.Title} />
+        </div>
+        
       </div>
-      <div className="card__info">
-        {strength && <p>Strength: <span>{strength} %</span></p>}
-      </div>
-    </li>
-  );
+    );
 };
 
 export default CocktailCard;
