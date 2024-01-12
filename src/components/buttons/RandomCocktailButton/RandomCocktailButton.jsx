71b8c3090370435a9d73e8615737403005380890
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2024-01-12 13:19:19 +0300

    fix: transfer functions from component to page

--- src/components/buttons/RandomCocktailButton/RandomCocktailButton.jsx
+++ src/components/buttons/RandomCocktailButton/RandomCocktailButton.jsx
@@ -1,31 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { useDispatch } from "react-redux";
-
-import { setCocktailID } from "../../../redux/slices/cocktailListSlice";
 
 //styles
 import styles from "./RandomCocktailButton.module.scss";
 
 const RandomCocktailButton = () => {
-  const dispatch = useDispatch();
-
-  function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  }
-
-  const handleClickRandomBtn = () => {
-    let randomNum = getRandomIntInclusive(0, 99)
-    dispatch(setCocktailID(randomNum))
-  }
- 
+  
   return (
     <Link
       to={`random-cocktail/random`}
       className={styles.button}
-      onClick={handleClickRandomBtn}
     >
       Random cocktail
     </Link>
--- src/pages/RandomCocktailPage.jsx
+++ src/pages/RandomCocktailPage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 //libraries
 import axios from "axios";
@@ -9,17 +9,26 @@ import CocktailCard from "../components/cocktailCard/CocktailCard";
 import SkeletonCocktailCard from "../components/cocktailCard/SkeletonCocktailCard";
 import BackButton from "../components/buttons/BackButton/BackButton";
 
+import { setCocktailID } from "../redux/slices/cocktailListSlice";
+
 const RandomCocktailPage = () => {
-  const {cocktailID} = useSelector((state) => state.cocktailList);
+  const dispatch = useDispatch();
   const [randomCocktail, setRandomCocktail] = useState(null);
-  
-  console.log('test')
+
+  function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
 
   useEffect(() => {
     axios
-    .get(`https://64f762d19d775408495385e6.mockapi.io/items`)
-    .then(({data}) => setRandomCocktail(data[cocktailID]))
-  }, [cocktailID]);
+      .get(`https://64f762d19d775408495385e6.mockapi.io/items`)
+      .then(({data}) => {
+        setRandomCocktail(data[getRandomIntInclusive(0, 99)]);
+        dispatch(setCocktailID(randomCocktail?.id))}
+      )
+  }, []);
 
   return (
     <div className="container cocktailPage">
