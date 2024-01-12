01d2f99003ba49ac033206943602ca652f43a564
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2024-01-12 12:42:44 +0300

    feat: create PaginationComponent

---
+++ src/components/pagination/PaginationComponent.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { setCurrentPage } from '../../redux/slices/paginationSlice';
+
+//styles
+import styles from './PaginationComponent.module.scss';
+
+const PaginationComponent = () => {
+  const {currentPage} = useSelector((state) => state.pagination);
+  const {countItems} = useSelector((state) => state.pagination);
+  const dispatch = useDispatch();
+
+  const itemsPerPage = 9;
+  const totalItems = countItems;
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
+
+  const pages = [];
+  for (let i = 1; i <= totalPages; i++) {
+    pages.push(i);
+  }
+
+  const handleClick = (page) => {
+    dispatch(setCurrentPage(page));
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      {pages.map(page => (
+        <button
+          key={page}
+          onClick={() => handleClick(page)}
+          className={`${styles.root} ${page === currentPage ? styles.current : null}`}
+        >
+          {page}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default PaginationComponent;
