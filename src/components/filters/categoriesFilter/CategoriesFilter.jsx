e8274a6b7346898e0468bc240a53d72f3eef5c3e
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2024-02-09 15:24:19 +0300

    fix: upgrade component CategoriesFilter

--- src/components/filters/categoriesFilter/CategoriesFilter.jsx
+++ src/components/filters/categoriesFilter/CategoriesFilter.jsx
@@ -1,45 +1,56 @@
-import { memo, useCallback } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { memo, useCallback } from "react";
 
-import { setCurrentCategory, setCurrentSubCategory, setBaseIngredient, setIngredientsOn } from '../../../redux/slices/filterSlice';
-import { setSorting } from '../../../redux/slices/sortingSlice';
-import { setCurrentPage } from '../../../redux/slices/paginationSlice';
+import { useSelector, useDispatch } from "react-redux";
 
-//styles
-import styles from './CategoriesFilter.module.scss';
+import {
+  setCurrentCategory,
+  setCurrentSubCategory,
+  setBaseIngredient,
+  setIngredientsOn,
+} from "../../../redux/slices/filterSlice";
+import { setSorting } from "../../../redux/slices/sortingSlice";
+import { setCurrentPage } from "../../../redux/slices/paginationSlice";
 
+import styles from "./CategoriesFilter.module.scss";
 
-const categories = ['All', 'Alcoholic', 'Non alcoholic'];
+const categories = ["All", "Alcoholic", "Non alcoholic"];
 
 const CategoriesFilter = memo(() => {
   const dispatch = useDispatch();
-  const currentCategory = useSelector((state) => state.filter.category);
 
-  const onChangeCategory = useCallback((category) => {
-    dispatch(setCurrentCategory(category));
-    if (category === 'Non alcoholic') {
-      dispatch(setSorting({ nameItem: 'popularity', nameSort: '' }));
-      dispatch(setCurrentSubCategory(null));
-    }
-    dispatch(setBaseIngredient(''));
-    dispatch(setIngredientsOn([]));
-    dispatch(setCurrentPage(1))
-  }, []);
+  const { currentCategory } = useSelector((state) => state.filter);
+
+  const onChangeCategory = useCallback(
+    (category) => {
+      dispatch(setCurrentCategory(category));
+      if (category === "Non alcoholic") {
+        dispatch(setSorting({ nameItem: "popularity", nameSort: "" }));
+        dispatch(setCurrentSubCategory(null));
+      }
+      dispatch(setBaseIngredient(""));
+      dispatch(setIngredientsOn([]));
+      dispatch(setCurrentPage(1));
+    },
+    [dispatch]
+  );
 
   return (
-    <div className='categories'>
+    <nav className="categories">
       <ul className={`${styles.categoriesList} list-reset`}>
-        {categories.map((category, index) => (
-          <li
-            key={index}
-            className={`${styles.categoriesList__item} ${currentCategory === category ? styles.current : '' }`}
-            onClick={() => onChangeCategory(category)}
-          >
-            {category}
+        {categories.map((category) => (
+          <li key={category}>
+            <button
+              className={`${styles.categoriesList__item} ${
+                currentCategory === category ? styles.current : ""
+              }`}
+              onClick={() => onChangeCategory(category)}
+            >
+              {category}
+            </button>
           </li>
         ))}
       </ul>
-    </div>
+    </nav>
   );
 });
 
