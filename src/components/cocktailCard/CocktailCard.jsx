8e72e464032353b8b8777720e0ccdb729f7d8954
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2024-02-09 12:21:37 +0300

    fix: upgrade component CocktailCard

--- src/components/cocktailCard/CocktailCard.jsx
+++ src/components/cocktailCard/CocktailCard.jsx
@@ -1,44 +1,53 @@
-import React from 'react';
+import React from "react";
 
-//components
-import CocktailProperty from '../cocktailProperty/CocktailProperty';
+import CocktailProperty from "../cocktailProperty/CocktailProperty";
 
-//styles
-import styles from './CocktailCard.module.scss';
+import styles from "./CocktailCard.module.scss";
 
-const CocktailCard = ({item}) => {
-    return (
-        <div className={styles.root}>
-        <div className={styles.root__info}>
-          <h1>{item.Title}</h1>
-          <div className={styles.root__ingredients}>
-            <h2>Ingredients</h2>
-            <ul className={`${styles.ingredientsList} list-reset`}>
-            {item.Ingredients?.map((el, i) => 
-              <li className={styles.ingredientsList__item} key={item.id + i}>
+const CocktailCard = ({ item }) => {
+  const {
+    Title,
+    Ingredients,
+    Instructions,
+    Alcoholic,
+    Category,
+    Glass,
+    totalStrength,
+    Img,
+    id,
+    Measures,
+  } = item;
+  return (
+    <div className={styles.root}>
+      <div className={styles.root__info}>
+        <h2>{Title}</h2>
+        <div className={styles.root__ingredients}>
+          <h3>Ingredients</h3>
+          <ul className={`${styles.ingredientsList} list-reset`}>
+            {Ingredients?.map((el, i) => (
+              <li className={styles.ingredientsList__item} key={`${id}-${i}`}>
                 <p className={styles.ingredientsList__name}>{el}</p>
-                <p className={styles.ingredientsList__amount}>{item.Measures[i]}</p>
+                <p className={styles.ingredientsList__amount}>{Measures?.[i]}</p>
               </li>
-            )}
-            </ul>
-          </div>
-          <div className={styles.root__instruction}>
-            <h2>Instructions</h2>
-            <p>{item.Instructions}</p>
-          </div>
-          <CocktailProperty title="Alcoholic" desc={item.Alcoholic} />
-          <CocktailProperty title="Category" desc={item.Category} />
-          <CocktailProperty title="Glass" desc={item.Glass} />
-          {item.totalStrength &&
-            <CocktailProperty title="TotalStrength" desc={item.totalStrength} />
-          }
+            ))}
+          </ul>
         </div>
-        <div className={styles.root__img}>
-          <img src={item.Img} alt={item.Title} />
+        <div className={styles.root__instruction}>
+          <h3>Instructions</h3>
+          <p>{Instructions}</p>
         </div>
-        
+        <CocktailProperty title="Alcoholic" desc={Alcoholic} />
+        <CocktailProperty title="Category" desc={Category} />
+        <CocktailProperty title="Glass" desc={Glass} />
+        {totalStrength && (
+          <CocktailProperty title="TotalStrength" desc={totalStrength} />
+        )}
       </div>
-    );
+      <div className={styles.root__img}>
+        <img src={Img} alt={Title} />
+      </div>
+    </div>
+  );
 };
 
 export default CocktailCard;
