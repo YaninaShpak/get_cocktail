cde149b2df2edd4ed4b540089b3c232499ceac7e
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2024-02-09 21:04:20 +0300

    fix: upgrade component CheckboxFilter
    - relocate ingredients to a separate file
    - use useCallback

--- src/components/filters/checkboxFilter/CheckboxFilter.jsx
+++ src/components/filters/checkboxFilter/CheckboxFilter.jsx
@@ -1,111 +1,44 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React, { useCallback } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
-import { setIngredientsOff, setIngredientsOn } from '../../../redux/slices/filterSlice';
-import { setCurrentPage } from '../../../redux/slices/paginationSlice';
+import {
+  setIngredientsOff,
+  setIngredientsOn,
+} from "../../../redux/slices/filterSlice";
+import { setCurrentPage } from "../../../redux/slices/paginationSlice";
 
-//styles
-import styles from './CheckboxFilter.module.scss';
-import CheckboxList from './CheckboxList/CheckboxList';
+import CheckboxList from "./CheckboxList/CheckboxList";
 
-const ingredients = {
-  alcohol: [
-    "Amaretto",
-    "Angostura Bitters",
-    "Aperol",
-    "Schnapps",
-    "Brandy",
-    "Cider",
-    "Applejack",
-    "Baileys irish cream",
-    "Banana Liqueur",
-    "Benedictine",
-    "Bitters",
-    "Blended whiskey",
-    "Curacao",
-    "Bourbon",
-    "Campari",
-    "Champagne",
-    "Coconut liqueur",
-    "Cognac",
-    "Cointreau",
-    "Liqueur",
-    "Dark rum",
-    "Gin", 
-    "Ginger ale", 
-    "Ginger beer",
-    "Jägermeister", 
-    "Kahlua",
-    "Kirschwasser",
-    "Lager",
-    "Light rum",  
-    "Malibu rum",
-    "Prosecco",
-    "Rum",
-    "Scotch",
-    "Sherry",
-    "Tequila",
-    "Tonic water",
-    "Vermouth",
-    "Vodka",
-    "Whiskey",
-    "White Wine",
-    "Red Wine",
-  ], 
-  nonAlcohol: [
-    "7-Up",
-    "Almond flavoring",
-    "Anise",
-    "Apple juice",
-    "Soda",
-    "Coca-Cola",
-    "Coffee",
-    "Cranberry Juice", 
-    "Cream",
-    "Egg White",
-    "Lemon Juice", 
-    "Lime Juice", 
-    "Fruit juice",
-    "Grapefruit juice",
-    "Grenadine",
-    "Hot chocolate",
-    "Lemonade",
-    "Milk",
-    "Orange Juice",
-    "Peach nectar",
-    "Pineapple Juice",
-    "Schweppes",
-    "Sprite",
-    "Tea",
-    "Ice-cream",
-  ]
-};
+import styles from "./CheckboxFilter.module.scss";
+
+import { ingredients } from "../../../data/ingredients";
 
 const CheckboxFilter = () => {
   const dispatch = useDispatch();
   const { currentCategory } = useSelector((state) => state.filter);
 
-  const excludeIngredients = (item) => {
+  const excludeIngredients = useCallback((item) => {
     dispatch(setIngredientsOff(item));
-
     dispatch(setCurrentPage(1));
-  }
+  }, [dispatch]);
 
-  const addIngredients = (item) => {
+  const addIngredients = useCallback((item) => {
     dispatch(setIngredientsOn(item));
     dispatch(setCurrentPage(1));
-  }
-  
+  }, [dispatch]);
+
   return (
     <div className={styles.checkboxFilter}>
       <h3 className={styles.title}>Add/Exclude ingredients</h3>
-      {currentCategory !== 'Non alcoholic' && <CheckboxList 
-        title="Alcohol"
-        list={ingredients.alcohol}
-        onClickExclude={excludeIngredients}
-        onClickAdd={addIngredients}
-      />}
-      <CheckboxList 
+      {currentCategory !== "Non alcoholic" && (
+        <CheckboxList
+          title="Alcohol"
+          list={ingredients.alcohol}
+          onClickExclude={excludeIngredients}
+          onClickAdd={addIngredients}
+        />
+      )}
+      <CheckboxList
         title="Non Alcohol"
         list={ingredients.nonAlcohol}
         onClickExclude={excludeIngredients}
