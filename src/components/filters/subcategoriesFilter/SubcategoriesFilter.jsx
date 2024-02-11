8d99d16586e1bbd48a49262622aca189ba01bc87
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2024-02-11 13:42:38 +0300

    fix: extract click handler into separate function

--- src/components/filters/subcategoriesFilter/SubcategoriesFilter.jsx
+++ src/components/filters/subcategoriesFilter/SubcategoriesFilter.jsx
@@ -1,30 +1,36 @@
-import React from 'react';
+import React, { useCallback } from "react";
 
-import { useSelector, useDispatch } from 'react-redux';
-import { setCurrentSubCategory } from '../../../redux/slices/filterSlice';
+import { useSelector, useDispatch } from "react-redux";
+import { setCurrentSubCategory } from "../../../redux/slices/filterSlice";
 
-//styles
-import styles from './SubcategoriesFilter.module.scss';
-
-
+import styles from "./SubcategoriesFilter.module.scss";
 
 const SubcategoriesFilter = () => {
-  const subcategories = ['low alcohol', 'strong'];
-  const {currentSubCategory} = useSelector((state) => state.filter);
+  const subcategories = ["low alcohol", "strong"];
+
   const dispatch = useDispatch();
+  const { currentSubCategory } = useSelector((state) => state.filter);
+
+  const handleItemClick = useCallback((item) => {
+    dispatch(setCurrentSubCategory(item));
+  }, [dispatch]);
 
   return (
     <div className={styles.root}>
       <h3 className={styles.title}>Strength</h3>
       <ul className={`${styles.list} list-reset`}>
-        {subcategories.map((item) => 
-        <li 
-          className={`${styles.listItem} ${item === currentSubCategory ? styles.current : ''}`}
-          key={item}
-          onClick={() => dispatch(setCurrentSubCategory(item))}
-        >
-          {item}
-        </li>)}
+        {subcategories.map((item) => (
+          <li key={item}>
+            <button
+              className={`${styles.listItem} ${
+                item === currentSubCategory ? styles.current : ""
+              }`}
+              onClick={() => handleItemClick(item)}
+            >
+              {item}
+            </button>
+          </li>
+        ))}
       </ul>
     </div>
   );
