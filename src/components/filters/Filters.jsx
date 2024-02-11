55d469b071c77ed0ca9d65c4a361bd567863aad7
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2024-02-11 13:49:51 +0300

    fix: upgrade component Filters

--- src/components/filters/Filters.jsx
+++ src/components/filters/Filters.jsx
@@ -1,18 +1,19 @@
-import { useSelector } from 'react-redux';
-
-//styles
-import styles from './Filters.module.scss';
+import { useSelector } from "react-redux";
 
 //components
-import CategoriesFilter from './categoriesFilter/CategoriesFilter';
-import RadioFilter from './radioFilter/RadioFilter';
-import RangeSlider from './rangeSlider/RangeSlider';
-import SubcategoriesFilter from './subcategoriesFilter/SubcategoriesFilter';
-import CheckboxFilter from './checkboxFilter/CheckboxFilter';
-import ClearButton from '../buttons/clearButton/ClearButton';
+import CategoriesFilter from "./categoriesFilter/CategoriesFilter";
+import RadioFilter from "./radioFilter/RadioFilter";
+import RangeSlider from "./rangeSlider/RangeSlider";
+import SubcategoriesFilter from "./subcategoriesFilter/SubcategoriesFilter";
+import CheckboxFilter from "./checkboxFilter/CheckboxFilter";
+import ClearButton from "../buttons/clearButton/ClearButton";
+
+import styles from "./Filters.module.scss";
 
 const Filters = () => {
   const { currentCategory } = useSelector((state) => state.filter);
+  const isAlcoholic = currentCategory === "Alcoholic";
+
   return (
     <section className={`${styles.filters} content-container__filters`}>
       <div className={styles.filters__titleWrapper}>
@@ -21,10 +22,14 @@ const Filters = () => {
       <div className={styles.filters__wrapper}>
         <CategoriesFilter />
         <RadioFilter />
-        {currentCategory === 'Alcoholic' && <RangeSlider/>}
-        {currentCategory === 'Alcoholic' && <SubcategoriesFilter/>}
-        <CheckboxFilter/>
-        <ClearButton/>
+        {isAlcoholic && (
+          <>
+            <RangeSlider />
+            <SubcategoriesFilter />
+          </>
+        )}
+        <CheckboxFilter />
+        <ClearButton />
       </div>
     </section>
   );
