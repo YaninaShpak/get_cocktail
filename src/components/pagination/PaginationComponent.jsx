dc66cedc3e90dd1b0d7dad763a05923e8f1bfd6f
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2024-02-12 11:45:29 +0300

    feat: add multipoint instead of a page group

--- src/components/pagination/PaginationComponent.jsx
+++ src/components/pagination/PaginationComponent.jsx
@@ -1,36 +1,55 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React, { useMemo } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
-import { setCurrentPage } from '../../redux/slices/paginationSlice';
+import { setCurrentPage } from "../../redux/slices/paginationSlice";
 
-//styles
-import styles from './PaginationComponent.module.scss';
+import styles from "./PaginationComponent.module.scss";
 
 const PaginationComponent = () => {
-  const {currentPage} = useSelector((state) => state.pagination);
-  const {countItems} = useSelector((state) => state.pagination);
   const dispatch = useDispatch();
+  const { currentPage, countItems } = useSelector((state) => state.pagination);
 
   const itemsPerPage = 9;
-  const totalItems = countItems;
-  const totalPages = Math.ceil(totalItems / itemsPerPage);
+  const totalPages = Math.ceil(countItems / itemsPerPage);
 
-  const pages = [];
-  for (let i = 1; i <= totalPages; i++) {
-    pages.push(i);
-  }
+  const pages = useMemo(() => {
+    const pages = [];
+    for (let i = 1; i <= totalPages; i++) {
+      // Всегда добавляем первую и последнюю страницу
+      if (i === 1 || i === totalPages) {
+        pages.push(i);
+      } 
+      // Добавляем 1 страницу перед и после текущей страницы
+      else if (i >= currentPage - 1 && i <= currentPage + 1) {
+        pages.push(i);
+      } 
+      // Добавляем многоточие вместо группы страниц
+      else if (i === currentPage - 2 || i === currentPage + 2) {
+        pages.push('...');
+      }
+    }
+    // Удаление повторяющихся многоточий
+    return pages.filter((page, index, self) => page !== '...' || self[index - 1] !== '...');
+
+    }, [totalPages, currentPage]
+  );
 
   const handleClick = (page) => {
-    dispatch(setCurrentPage(page));
+    if (page !== '...') {
+      dispatch(setCurrentPage(page));
+    }
   };
 
   return (
     <div className={styles.wrapper}>
-      {pages.map(page => (
+      {pages.map((page, i) => (
         <button
-          key={page}
+          key={i}
           onClick={() => handleClick(page)}
-          className={`${styles.root} ${page === currentPage ? styles.current : null}`}
+          className={`${styles.root} ${
+            page === currentPage ? styles.current : null
+          }`}
+          disabled={page === '...'}
         >
           {page}
         </button>
