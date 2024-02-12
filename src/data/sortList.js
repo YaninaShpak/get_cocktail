109a290767ba3037da8a6ff27db7b3963d116b5c
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2024-02-12 19:59:14 +0300

    feat: create sortList.js

---
+++ src/data/sortList.js
@@ -0,0 +1,6 @@
+export const sortList = [
+  { nameItem: 'popularity', nameSort: '' },
+  { nameItem: 'alphabet', nameSort: 'title' },
+  { nameItem: 'strength ↑', nameSort: 'totalStrength', order: 'desc' },
+  { nameItem: 'strength ↓', nameSort: 'totalStrength', order: 'asc'}
+];
