cd7439ca0f8e6c29e6a845efa5df6369da0131aa
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2024-02-12 19:59:31 +0300

    refactor: format index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
 
-import { store } from './redux/store';
-import { Provider } from 'react-redux';
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
 
-import App from './App';
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <BrowserRouter>
