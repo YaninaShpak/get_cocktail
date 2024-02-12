0d670643a73491948de01ef93ec392fd242029c6
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2024-02-12 11:43:23 +0300

    refactor: format component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-//styles
-import style from './Header.module.scss';
+import style from "./Header.module.scss";
 
 const Header = () => {
   return (
     <header className={style.mainHeader}>
       <p className={style.mainHeader__title}>
-        <Link className="link" to="/">Let's get cocktail!</Link>
+        <Link className="link" to="/">
+          Let's get cocktail!
+        </Link>
       </p>
     </header>
   );
