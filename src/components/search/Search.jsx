b41a9ef85923777b895abcad89692c65075e1c20
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2024-02-12 19:54:57 +0300

    refactor: format Search.jsx

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,35 +1,39 @@
-import React, { useCallback, useRef, useState } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useCallback, useRef, useState } from "react";
+import { useDispatch } from "react-redux";
 
-import debounce from 'lodash.debounce';
+import { setSearchValue } from "../../redux/slices/searchSlice";
+import { setCurrentPage } from "../../redux/slices/paginationSlice";
 
-import { setSearchValue } from '../../redux/slices/searchSlice';
-import { setCurrentPage } from '../../redux/slices/paginationSlice';
+import debounce from "lodash.debounce";
 
-//styles
-import styles from './Search.module.scss';
+import ClearIcon from "../icons/clearIcon/ClearIcon";
+
+import styles from "./Search.module.scss";
 
 const Search = () => {
-  const [value, setValue] = useState('');
   const dispatch = useDispatch();
-  const inputRef = useRef();
+  const [value, setValue] = useState("");
 
-  const clearSearch = () => {
-    dispatch(setSearchValue(''));
-    setValue('');
-    inputRef.current?.focus();
-  }
+  const inputRef = useRef();
 
   const updatedSearchValue = useCallback(
     debounce((str) => {
       dispatch(setSearchValue(str));
-      dispatch(setCurrentPage(1))
-    }, 800), []);
+      dispatch(setCurrentPage(1));
+    }, 800),
+    []
+  );
 
-  const onChangeSearch = (e) => {
-    setValue(e.target.value);
-    updatedSearchValue(e.target.value);
-  }
+  const onChangeSearch = ({ target: { value } }) => {
+    setValue(value);
+    updatedSearchValue(value);
+  };
+
+  const clearSearch = () => {
+    dispatch(setSearchValue(""));
+    setValue("");
+    inputRef.current?.focus();
+  };
 
   return (
     <div className={styles.root}>
@@ -40,16 +44,9 @@ const Search = () => {
         className={styles.input}
         id="search"
         type="text"
-        placeholder='Search'
+        placeholder="Search"
       />
-      {value &&
-        <svg
-          className={styles.iconClear} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"
-          onClick={clearSearch}
-        >
-          <path d="M5.72 5.72a.75.75 0 0 1 1.06 0L12 10.94l5.22-5.22a.749.749 0 0 1 1.275.326.749.749 0 0 1-.215.734L13.06 12l5.22 5.22a.749.749 0 0 1-.326 1.275.749.749 0 0 1-.734-.215L12 13.06l-5.22 5.22a.751.751 0 0 1-1.042-.018.751.751 0 0 1-.018-1.042L10.94 12 5.72 6.78a.75.75 0 0 1 0-1.06Z"></path>
-        </svg>
-      }
+      {value && <ClearIcon onClick={clearSearch} />}
     </div>
   );
 };
