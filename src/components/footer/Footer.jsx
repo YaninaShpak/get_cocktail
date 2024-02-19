a078be12365b60dc7ab48dad156a7c71aa78dbf8
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2024-02-19 10:51:16 +0300

    feat: create component Footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import styles from './Footer.module.scss';
+
+const Footer = () => {
+  return (
+    <footer className={styles.root}>
+      <div className="container">
+        <p className={styles.copyright}>© 2024 · Ianina Shpak</p>
+      </div>
+      
+    </footer>
+  );
+};
+
+export default Footer;
