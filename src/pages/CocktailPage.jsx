ef9ad749867a4b4922133864e338f78080b04431
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2024-04-16 10:22:02 +0300

    feat: delete BackButton

--- src/pages/CocktailPage.jsx
+++ src/pages/CocktailPage.jsx
@@ -4,7 +4,6 @@ import { useSelector } from "react-redux";
 //components
 import CocktailCard from "../components/cocktailCard/CocktailCard";
 import SkeletonCocktailCard from "../components/cocktailCard/SkeletonCocktailCard";
-import BackButton from "../components/buttons/backButton/BackButton";
 
 const CocktailPage = () => {
   const { items, cocktailID } = useSelector((state) => state.cocktailList);
@@ -14,7 +13,6 @@ const CocktailPage = () => {
   return (
     <div className="container cocktailPage">
       {item ? <CocktailCard item={item} /> : <SkeletonCocktailCard />}
-      <BackButton />
     </div>
   );
 };
