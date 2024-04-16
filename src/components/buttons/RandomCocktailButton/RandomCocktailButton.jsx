07481a3fd8cd2da426b338f79ff8aac427571ee5
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2024-04-16 12:01:01 +0300

    fix: change logic of getting a random cocktail

--- src/components/buttons/RandomCocktailButton/RandomCocktailButton.jsx
+++ src/components/buttons/RandomCocktailButton/RandomCocktailButton.jsx
@@ -2,15 +2,24 @@ import React, { memo } from "react";
 import { Link } from "react-router-dom";
 import PropTypes from 'prop-types';
 
+import { useDispatch } from "react-redux";
+import { setRandomNum } from "../../../redux/slices/cocktailListSlice";
+
+import getRandomIntInclusive from "../../../utils/getRandomNum";
+
 import styles from "./RandomCocktailButton.module.scss";
 
-const RandomCocktailButton = ({onClick}) => {
+const RandomCocktailButton = ({ onClick }) => {
+  const dispatch = useDispatch();
   
   return (
     <Link
       to="random-cocktail/random/:title"
       className={`${styles.button} actionsWrapper__button`}
-      onClick={onClick}
+      onClick={() => {
+        // onClick();
+        dispatch(setRandomNum(getRandomIntInclusive(0, 99)));
+      }}
     >
       Random cocktail
     </Link>
--- src/pages/RandomCocktailPage.jsx
+++ src/pages/RandomCocktailPage.jsx
@@ -1,35 +1,28 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-
-import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
 import axios from "axios";
-
-import getRandomIntInclusive from "../utils/getRandomNum";
 
 //components
 import CocktailCard from "../components/cocktailCard/CocktailCard";
 import SkeletonCocktailCard from "../components/cocktailCard/SkeletonCocktailCard";
 
-import { setCocktailID } from "../redux/slices/cocktailListSlice";
-
 const RandomCocktailPage = () => {
   const navigate = useNavigate();
-  const dispatch = useDispatch();
+  const { randomNum } = useSelector((state) => state.cocktailList);
   const [randomCocktail, setRandomCocktail] = useState(null);
 
   useEffect(() => {
     axios
-      .get(`https://64f762d19d775408495385e6.mockapi.io/cocktails`)
-      .then(({ data }) => {
-        const selectedCocktail = data[getRandomIntInclusive(0, 99)];
-        setRandomCocktail(selectedCocktail);
-        dispatch(setCocktailID(selectedCocktail?.Title));
-        navigate(`/random-cocktail/random/${selectedCocktail?.Title}`);
-      });
-  }, []);
-
-  
+    .get(`https://64f762d19d775408495385e6.mockapi.io/cocktails`)
+    .then(({ data }) => {
+      const selectedCocktail = data[randomNum];
+      console.log(selectedCocktail)
+      setRandomCocktail(selectedCocktail);
+      navigate(`/random-cocktail/random/${selectedCocktail?.Title}`);
+    });
+  },[randomNum])
 
   return (
     <div className="container cocktailPage">
