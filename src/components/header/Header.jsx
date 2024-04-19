d5f4c63496500f72f497837648e6eb7847f9d70c
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2024-04-19 15:48:54 +0300

    fix: delete saveFiltersToLocalStorage from Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,32 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import { useSelector } from "react-redux";
-
 import RandomCocktailButton from "../buttons/randomCocktailButton/RandomCocktailButton";
-
-import saveToLocalStorage from "../../utils/saveToLocalStorage";
 
 import style from "./Header.module.scss";
 
 const Header = () => {
-  const {
-    currentCategory,
-    currentSubCategory,
-    baseIngredient,
-    excludeIngredients
-  } = useSelector((state) => state.filter);
-  const { sorting } = useSelector((state) => state.sort);
-  const { currentPage } = useSelector((state) => state.pagination);
-
-  const saveFiltersToLocalStorage = () => {
-    saveToLocalStorage("currentCategory", currentCategory);
-    saveToLocalStorage("currentSubCategory", currentSubCategory);
-    saveToLocalStorage("baseIngredient", baseIngredient);
-    saveToLocalStorage("excludeIngredients", excludeIngredients);
-    saveToLocalStorage("currentPage", currentPage);
-    saveToLocalStorage("sorting", sorting);
-  }
   
   return (
     <header className={style.mainHeader}>
@@ -37,7 +16,7 @@ const Header = () => {
               Let's get cocktail!
             </Link>
           </p>
-          <RandomCocktailButton onClick={saveFiltersToLocalStorage} />
+          <RandomCocktailButton />
         </div>
       </div>
     </header>
