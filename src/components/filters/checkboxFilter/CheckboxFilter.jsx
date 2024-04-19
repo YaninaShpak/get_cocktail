7d0e92a7cb9032bed1650aded1aeb8fd48d927f3
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2024-04-19 15:38:45 +0300

    feat: delete of creation newIngredientsList

--- src/components/filters/checkboxFilter/CheckboxFilter.jsx
+++ src/components/filters/checkboxFilter/CheckboxFilter.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
 
 import CheckboxList from "./CheckboxList/CheckboxList";
@@ -9,43 +9,14 @@ import { ingredients } from "../../../data/ingredients";
 
 const CheckboxFilter = () => {
   const { currentCategory } = useSelector((state) => state.filter);
-  const {ingredientsList} = useSelector((state) => state.ingredientsList);
-
-  const [newIngredientsList, setNewIngredientsList] = useState({
-    alcohol: [],
-    nonAlcohol: []
-  });
-  
-  useEffect(() => {
-    const updatedAlcohol = ingredients.alcohol.filter((el) => ingredientsList?.some((item) => item.toLowerCase().includes(el.toLowerCase())));
-    const updatedNonAlcohol = ingredients.nonAlcohol.filter((el) => ingredientsList?.some((item) => item.toLowerCase().includes(el.toLowerCase())));
-    
-    setNewIngredientsList({
-      ...newIngredientsList,
-      alcohol: updatedAlcohol,
-      nonAlcohol: updatedNonAlcohol
-    });
-
-    console.log(newIngredientsList)
-  }, [ingredientsList]);
-
-  const isItems = (array) => {
-    if (array.length > 0) {
-      return array;
-    } else {
-      return null
-    }
-  }
-  
-  console.log(ingredientsList)
 
   return (
     <div className={styles.checkboxFilter}>
       <h3 className={styles.title}>Add/Exclude ingredients</h3>
       {currentCategory !== "Non alcoholic" && (
-        <CheckboxList title="Alcohol" list={isItems(newIngredientsList.alcohol) || ingredients.alcohol} />
+        <CheckboxList title="Alcohol" list={ingredients.alcohol} />
       )}
-      <CheckboxList title="Non Alcohol" list={isItems(newIngredientsList.nonAlcohol) || ingredients.nonAlcohol} />
+      <CheckboxList title="Non Alcohol" list={ingredients.nonAlcohol} />
     </div>
   );
 };
