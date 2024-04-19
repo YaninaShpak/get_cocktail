a93ac1e5da1b0bf52b53d90dd5170a86d4b87346
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2024-04-19 15:34:02 +0300

    feat: delete props

--- src/components/buttons/RandomCocktailButton/RandomCocktailButton.jsx
+++ src/components/buttons/RandomCocktailButton/RandomCocktailButton.jsx
@@ -1,6 +1,5 @@
-import React, { memo } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import PropTypes from 'prop-types';
 
 import { useDispatch } from "react-redux";
 import { setRandomNum } from "../../../redux/slices/cocktailListSlice";
@@ -9,25 +8,18 @@ import getRandomIntInclusive from "../../../utils/getRandomNum";
 
 import styles from "./RandomCocktailButton.module.scss";
 
-const RandomCocktailButton = ({ onClick }) => {
+const RandomCocktailButton = () => {
   const dispatch = useDispatch();
   
   return (
     <Link
       to="random-cocktail/random/:title"
       className={`${styles.button} actionsWrapper__button`}
-      onClick={() => {
-        // onClick();
-        dispatch(setRandomNum(getRandomIntInclusive(0, 99)));
-      }}
+      onClick={() => dispatch(setRandomNum(getRandomIntInclusive(0, 99)))}
     >
       Random cocktail
     </Link>
   );
 };
 
-RandomCocktailButton.propTypes = {
-  onClick: PropTypes.func.isRequired,
-}
-
-export default memo(RandomCocktailButton);
+export default RandomCocktailButton;
