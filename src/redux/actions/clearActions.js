381370b4d61f3fd0b9cef438cd9a0d7c9ef651df
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2024-04-19 15:51:16 +0300

    feat: add setIncludeIngredients

--- src/redux/actions/clearActions.js
+++ src/redux/actions/clearActions.js
@@ -4,17 +4,21 @@ import {
   setCurrentCategory,
   setCurrentSubCategory,
   setBaseIngredient,
-  setExcludeIngredients
+  setIncludeIngredients,
+  setExcludeIngredients,
 } from "../slices/filterSlice";
 import { setValueMin, setValueMax } from "../slices/rangeSliderSlice";
+import { setIngredientsList } from "../slices/ingredientsSlice";
 
 export const clearFiltersAndRange = () => (dispatch) => {
   batch(() => {
     dispatch(setCurrentCategory("All"));
     dispatch(setCurrentSubCategory(null));
     dispatch(setBaseIngredient(""));
+    dispatch(setIncludeIngredients([]));
     dispatch(setExcludeIngredients([]));
     dispatch(setValueMin(3));
     dispatch(setValueMax(100));
+    dispatch(setIngredientsList(null))
   });
 };
