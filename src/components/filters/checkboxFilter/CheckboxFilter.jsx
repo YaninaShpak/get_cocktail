c37c6ecf40d83fd3571c429dcb804683ee85b43f
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2024-04-19 14:42:08 +0300

    feat: create newIngredientsList to CheckboxFilter

--- src/components/filters/checkboxFilter/CheckboxFilter.jsx
+++ src/components/filters/checkboxFilter/CheckboxFilter.jsx
@@ -1,6 +1,5 @@
-import React from "react";
-import { useSelector, useDispatch} from "react-redux";
-import { setExcludeIngredients } from "../../../redux/slices/filterSlice";
+import React, { useEffect, useRef, useState } from "react";
+import { useSelector } from "react-redux";
 
 import CheckboxList from "./CheckboxList/CheckboxList";
 
@@ -9,33 +8,44 @@ import styles from "./CheckboxFilter.module.scss";
 import { ingredients } from "../../../data/ingredients";
 
 const CheckboxFilter = () => {
-  const dispatch = useDispatch();
-  const { currentCategory, excludeIngredients } = useSelector((state) => state.filter);
+  const { currentCategory } = useSelector((state) => state.filter);
+  const {ingredientsList} = useSelector((state) => state.ingredientsList);
 
-  const handleExcludeAllClick = () => {
-    if (excludeIngredients.length === 0) {
-      dispatch(setExcludeIngredients(ingredients.alcohol.concat(ingredients.nonAlcohol)));
+  const [newIngredientsList, setNewIngredientsList] = useState({
+    alcohol: [],
+    nonAlcohol: []
+  });
+  
+  useEffect(() => {
+    const updatedAlcohol = ingredients.alcohol.filter((el) => ingredientsList?.some((item) => item.toLowerCase().includes(el.toLowerCase())));
+    const updatedNonAlcohol = ingredients.nonAlcohol.filter((el) => ingredientsList?.some((item) => item.toLowerCase().includes(el.toLowerCase())));
+    
+    setNewIngredientsList({
+      ...newIngredientsList,
+      alcohol: updatedAlcohol,
+      nonAlcohol: updatedNonAlcohol
+    });
+
+    console.log(newIngredientsList)
+  }, [ingredientsList]);
+
+  const isItems = (array) => {
+    if (array.length > 0) {
+      return array;
     } else {
-      dispatch(setExcludeIngredients([]));
+      return null
     }
-  };
+  }
+  
+  console.log(ingredientsList)
 
   return (
     <div className={styles.checkboxFilter}>
       <h3 className={styles.title}>Add/Exclude ingredients</h3>
-      <div className={styles.buttonWrapper} onClick={handleExcludeAllClick}>
-        <div className={`${styles.checkbox} ${excludeIngredients.length !== 0 ? styles.hide : ''}`}>
-          <span className="material-icons">done</span>
-        </div>
-        <button className={styles.button} type="button">
-          Add/Exclude All
-        </button>
-      </div>
-
       {currentCategory !== "Non alcoholic" && (
-        <CheckboxList title="Alcohol" list={ingredients.alcohol} />
+        <CheckboxList title="Alcohol" list={isItems(newIngredientsList.alcohol) || ingredients.alcohol} />
       )}
-      <CheckboxList title="Non Alcohol" list={ingredients.nonAlcohol} />
+      <CheckboxList title="Non Alcohol" list={isItems(newIngredientsList.nonAlcohol) || ingredients.nonAlcohol} />
     </div>
   );
 };
